e273be956a055e710891745d4d98caa508c0377c
Prateek Singhal <m22aie215@iitj.ac.in>
2023-08-12 00:28:58 +0530

    first commit

--- setup.py
+++ setup.py
@@ -4,6 +4,7 @@ Hello World app for running Python apps on Bluemix
 
 # Always prefer setuptools over distutils
 from setuptools import setup, find_packages
+
 # To use a consistent encoding
 from codecs import open
 from os import path
@@ -11,14 +12,15 @@ from os import path
 here = path.abspath(path.dirname(__file__))
 
 # Get the long description from the README file
-with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+with open(path.join(here, "README.md"), encoding="utf-8") as f:
     long_description = f.read()
 
 setup(
-    name='python-hello-world-flask',
-    version='1.0.0',
-    description='Hello World app for running Python apps on Bluemix',
+    name="python-hello-world-flask",
+    version="1.0.0",
+    description="Hello World app for running Python apps on Bluemix",
     long_description=long_description,
-    url='https://github.com/IBM-Bluemix/python-hello-world-flask',
-    license='Apache-2.0'
+    url="https://github.com/IBM-Bluemix/python-hello-world-flask",
+    license="Apache-2.0",
 )
+# checking git comments
